42caff7aeed3aeb4aa1d4a001d2d8cbde34e2cda
Cody Casselman <cody.casselman@ucalgary.ca>
2025-02-28 19:05:48 -0700

    Add: refactored event.ts to reduce duplicated code

--- src/events.ts
+++ src/events.ts
@@ -2,6 +2,7 @@ import { Game } from "./classes/game";
 import { PADDLE_HEIGHT, PADDLE_SPEED } from "./constants";
 import { GameState } from "./types";
 import { drawMenu, menuActions, moveMenuCursor } from "./ui/menu";
+import { Player } from "./classes/player";
 
 // destructuring the game instance
 const game = Game.getInstance();
@@ -29,53 +30,59 @@ document.addEventListener("click", function (_event: Event) {
 document.addEventListener("keydown", function (event: KeyboardEvent) {
   handleMovement(event); 
 
-  if (event.code === "Space" && game.gameState === GameState.menu) {
-    // Start a new game loop
-    game.newGame();
-  } else if (event.code === "Escape") {
+  if(game.gameState === GameState.menu){
+    if (event.code === "Space") {
+      // Start a new game loop
+      game.newGame();
+    } 
+    else if (event.code === "Enter") {
+      // If the Enter key is pressed in menu, do something based on the selected menu option
+      menuActions();
+    }
+  }
+  else if (event.code === "Escape") {
     // Draw the menu
     console.log("ESCAPE");
     drawMenu();
-  } else if (event.code === "Enter" && game.gameState === GameState.menu) {
-    // If the Enter key is pressed in menu, do something based on the selected menu option
-    menuActions();
-  }
+  } 
+  
 });
+
 //REFACTOR
 const handleMovement = (event: KeyboardEvent): void =>{
-  //Moving Player 1
-  if (event.code === "KeyW") {
-    game.player1.velocityY = -PADDLE_SPEED;
-  } else if (event.code === "KeyS") {
-    game.player1.velocityY = PADDLE_SPEED;
-  } 
-  //Moving Player 2
-  else if (event.code === "ArrowUp") {
-    if (game.gameState === GameState.menu) {
-      event.preventDefault();
-      moveMenuCursor("up");
+  //Handling Key Input
+  if (event.code === "KeyW" || event.code ==="KeyS") {
+    let paddleOneSpeed = (event.code === "KeyW" ? -PADDLE_SPEED : PADDLE_SPEED);
+    movePlayer(game.player1, paddleOneSpeed);
+  }
+  //Handling arrow input
+  else if (event.code === "ArrowUp" || event.code === "ArrowDown") {
+    let direction: string = (event.code === "ArrowUp" ? "up" : "down");
+    let paddleTwoSpeed: number = (event.code === "ArrowUp" ? -PADDLE_SPEED : PADDLE_SPEED);
+    if(game.gameState === GameState.menu){
+      event.preventDefault
+      moveMenuCursor(direction)
     }
-    // Move player2 up
-    game.player2.velocityY = -PADDLE_SPEED;
-  } else if (event.code === "ArrowDown") {
-    if (game.gameState === GameState.menu) {
-      event.preventDefault();
-      moveMenuCursor("down");
-    }
-    // Move player2 down
-    game.player2.velocityY = PADDLE_SPEED;
+    movePlayer(game.player2, paddleTwoSpeed);
   }
 }
+
+const movePlayer = (player: Player, speed: number): void => {
+  player.velocityY = speed;
+}
+
+
+
 // Stop the player from moving
 document.addEventListener("keyup", function (event: KeyboardEvent) {
   // Player 1 controls
   if (event.code === "KeyW" || event.code === "KeyS") {
     // Stop player1 movement
-    game.player1.velocityY = 0;
+    movePlayer(game.player1, 0);
   }
   // Player 2 controls
   else if (event.code === "ArrowUp" || event.code === "ArrowDown") {
     // Stop player2 movement
-    game.player2.velocityY = 0;
+    movePlayer(game.player2, 0);
   }
 });
